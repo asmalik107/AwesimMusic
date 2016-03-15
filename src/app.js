1e63389ce332a8fa5c1ac95cc8483486007d66fd
asmalik107 <asmalik@outlook.com>
2016-03-15 11:15:02 +0000

    Add tab bar

--- src/app.js
+++ src/app.js
@@ -2,20 +2,50 @@ import React,{
     Component,
     StyleSheet,
     View,
-    Text
+    Text,
+    TabBarIOS
 } from 'react-native';
 
 
 class App extends Component {
     constructor() {
         super();
+
+        this.state = {
+            selectedTab: 'releases'
+        }
+    }
+
+
+    setSelectedTab(tabId) {
+        this.setState({selectedTab: tabId});
     }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text> App</Text>
-            </View>
+            <TabBarIOS>
+                <TabBarIOS.Item
+                    title="New Releases"
+                    selected={this.state.selectedTab === 'releases'}
+                    onPress={() => this.setSelectedTab('releases')}
+                >
+                    <Text>Test1</Text>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    title="PlayLists"
+                    selected={this.state.selectedTab === 'playlists'}
+                    onPress={() => this.setSelectedTab('playlists')}
+                >
+                    <Text>Test1</Text>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    title="Settings"
+                    selected={this.state.selectedTab === 'settings'}
+                    onPress={() => this.setSelectedTab('settings')}
+                >
+                    <Text>Test2</Text>
+                </TabBarIOS.Item>
+            </TabBarIOS>
         );
     }
 }
